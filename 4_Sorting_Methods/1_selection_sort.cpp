581e76a552dec08c50b6eb17027dda51b7437306
Nayan <codingonly2029@gmail.com>
2026-04-11 11:09:48 +0530

    sorting codes added

---
+++ 4_Sorting_Methods/1_selection_sort.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+    for(int i=0; i<n-1; i++){
+        int minindex = i;
+        
+        for(int j=i+1; j<n; j++){
+            if(arr[j] < arr[minindex]){
+                minindex = j;
+            }
+        }
+        
+        swap(arr[minindex] , arr[i]);
+
+        // int temp = arr[minindex];
+        // arr[minindex] = arr[i];
+        // arr[i] = temp;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n ; i++){
+        cin >> arr[i];
+    }
+
+    selectionSort(arr , n);
+
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
