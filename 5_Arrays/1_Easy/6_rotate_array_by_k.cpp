d311ed514b3a9d2bb8069ef3b4da03ed1f1e2e31
Nayan <codingonly2029@gmail.com>
2026-04-12 18:49:53 +0530

    Update 6_rotate_array_by_k.cpp

---
+++ 5_Arrays/1_Easy/6_rotate_array_by_k.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void rotate(vector<int>& nums, int k, int n) {
+    k = k % n;
+
+    // case 1: k < n → k%n = k → k unchanged → runs normally
+    // case 2: k = n → k%n = 0 → returns immediately → no rotation
+    // case 3: k > n → k%n = remainder → k reduced → runs with new k
+
+    if (k == 0) return;
+
+    vector<int> temp(k);
+
+    for (int i = 0; i < k; i++)
+        temp[i] = nums[n-k+i];
+
+    for (int i = n-1; i >= k; i--)
+        nums[i] = nums[i-k];
+
+    for (int i = 0; i < k; i++)
+        nums[i] = temp[i];
+}
+
+int main(){
+    int n,k;
+    cout << "Enter size of array : " << endl;
+    cin >> n;
+
+    cout << "Enter k : "<< endl;
+    cin >> k;
+
+    vector<int> nums(n);
+    for(int i=0; i<n; i++){
+        cin >> nums[i];
+    }
+
+    rotate(nums , k , n);
+
+    for(int i=0; i<n; i++){
+        cout << nums[i]<<" ";
+    }
+
+    return 0;
+}
