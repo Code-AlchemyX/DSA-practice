f9bdd1e1c5a1e015c66598dafc3a08769e1c63a8
Nayan <codingonly2029@gmail.com>
2026-05-25 09:53:36 +0530

    Enhance GCD function with explanatory comments
    Added comments explaining the GCD calculation using the Euclidean algorithm.

--- 1_Basic_Maths/4_gcd.cpp
+++ 1_Basic_Maths/4_gcd.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 // Search GCD EULCLIDEAN LOGIC on yt for better understanding. [ channel - Learn Math Tutorials ]
+// a = bq + r   ---> q = quotient and r = remainder(a%b)
+// so b shifts to a then r shifts to b. Therefore a = b and b = r ( temp = a%b ). And at last when the number gets completely divided r = 0 , b = gcd --> Then the final shift takes place where a = b(gcd) and b = r = 0. As b=0 the loop stops.
 int gcd(int a, int b) {
     while(b!=0){
         int temp = a%b;
