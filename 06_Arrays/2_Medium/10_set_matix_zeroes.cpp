ba49866e65b503182c0d4c836d1ee69ecff8f49a
Nayan <codingonly2029@gmail.com>
2026-06-02 13:40:03 +0530

    .

--- 06_Arrays/2_Medium/10_set_matix_zeroes.cpp
+++ 06_Arrays/2_Medium/10_set_matix_zeroes.cpp
@@ -1,4 +1,7 @@
-//! ====================================== Brute Force ======================================
+//! ====================================== Brute Force ========================================
+//* Time Complexity = O(m x n x (m+n)) 
+//* Space Complexity = O(m+n)
+
 
 class Solution {
 public:
@@ -32,5 +35,43 @@ public:
     }
 };
 
+//! ===================================== Better Approach --> O(n^2) =====================================
+//* Time Complexity = O(m x n) 
+//* Space Complexity = O(m+n)
+
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> row(m,0);
+        vector<int> col(n,0);
+    
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if( matrix[i][j] == 0 ){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if( row[i] == 1 || col[j] == 1 ){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+    }
+};
+
 
 //! ====================================== Optimal Approach ======================================
+//* Time Complexity = O(m x n) 
+//* Space Complexity = O(1)
+
+
