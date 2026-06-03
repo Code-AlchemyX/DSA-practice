8bb6518910d38d0cf20d2bc4231d5bd554ef0af0
Nayan <codingonly2029@gmail.com>
2026-06-03 10:27:08 +0530

    Update 5_quick_sort.cpp

---
+++ 05_Sorting_Methods/5_quick_sort.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    void quickSort(vector<int>& arr, int low, int high) {
+        if(low < high){
+            int partition_index = partition(arr, low, high);
+            quickSort(arr, low, partition_index-1);
+            quickSort(arr, partition_index+1, high);
+        }
+    }
+
+    int partition(vector<int>& arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+        
+        while(i < j){
+            while(arr[i] <= pivot && i <= high-1){
+                i++;
+            }
+            while(arr[j] > pivot && j >= low+1){
+                j--;
+            }
+            if(i < j){
+                swap(arr[i], arr[j]);
+            }
+        }
+        
+        swap(arr[low], arr[j]);
+        return j;
+    }
+};
