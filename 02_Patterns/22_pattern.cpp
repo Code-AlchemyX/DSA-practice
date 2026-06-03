1587875f915bb999b47905c9d66a1c20a9c4c6e8
Nayan <codingonly2029@gmail.com>
2026-06-03 13:02:04 +0530

    .

--- 02_Patterns/22_pattern.cpp
+++ 02_Patterns/22_pattern.cpp
@@ -8,6 +8,43 @@
 //       5 4 4 4 4 4 4 4 5 
 //       5 5 5 5 5 5 5 5 5
 
+//!=================================== EASY UNDERSTANDABLE METHOD ===================================
+#include<bits/stdc++.h>
+using namespace std;
+
+class Pattern
+{
+public:
+    void pattern22(int n){
+        for(int i=0; i<2*n-1; i++){
+            for(int j=0; j<2*n-1; j++){
+                int top = i;
+                int bottom = 2*(n-1) - i;
+                int left = j;
+                int right = 2*(n-1) - j;
+            
+                int minimum = min({top, bottom, left, right});
+                int value = n - minimum;
+                cout << value << " ";    
+            }
+            cout << endl;
+        } 
+    }
+};
+
+int main()
+{
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+
+    Pattern p;
+    p.pattern22(n);
+
+    return 0;
+}
+
+//!============== METHOD 2 - CAN BE UNDERSTOOD ONCE ABOVE METHOD IS CRYSTAL CLEARV==============
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -16,7 +53,13 @@ class Pattern
 {
 public:
     void pattern22(int n){
-        
+        for(int i=0; i<2*n-1; i++){
+            for(int j=0; j<2*n-1; j++){
+                int value = n - min({i , j , 2*(n-1)-i , 2*(n-1)-j});
+                cout << value << " ";
+            }
+            cout << endl;
+        } 
     }
 };
 
