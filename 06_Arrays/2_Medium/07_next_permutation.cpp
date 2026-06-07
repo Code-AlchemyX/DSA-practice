1fcad908e2726f297f5e1d1f6ed0a77ab8d8fa87
Nayan <codingonly2029@gmail.com>
2026-06-07 22:05:31 +0530

    .

--- 06_Arrays/2_Medium/07_next_permutation.cpp
+++ 06_Arrays/2_Medium/07_next_permutation.cpp
@@ -1,5 +1,8 @@
 //! ============================================== Brute Force ==============================================
-Generate all permutations of the array using recursion and backtracking. Sort them lexicographically. Find the current array in the sorted list. Return the permutation at the next index. If current is the last permutation, return the first (wrap around using modulo).
+Generate all permutations of the array using recursion and backtracking. 
+Sort them lexicographically.
+Find the current array in the sorted list. Return the permutation at the next index.
+If current is the last permutation, return the first (wrap around using modulo).
 
 TC = O(n! × n), Space = O(n!).
 
@@ -8,3 +11,32 @@ TC = O(n! × n), Space = O(n!).
 next_permutation(nums.begin(), nums.end());
 
 //! ============================================ Optimal Approach ===========================================
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int index = -1;
+
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+
+        if( index == -1 ){
+            reverse(nums.begin() , nums.end());
+            return;
+        }
+
+        for(int i=n-1; i>index; i--){
+            if(nums[i] > nums[index]){
+                swap( nums[index] , nums[i] );
+                break;
+            }
+        }
+
+        reverse(nums.begin() + 1 + index , nums.end());
+    }
+};
--- 06_Arrays/2_Medium/13_count_subarrays_with_given_sum.cpp
+++ 06_Arrays/2_Medium/13_count_subarrays_with_given_sum.cpp
@@ -7,12 +7,10 @@ public:
         int count = 0;
 
         for(int i=0; i<n; i++){
-            int current_sum = 0;
-
+            int sum = 0;
             for(int j=i; j<n; j++){
-                current_sum += nums[j];
-
-                if( current_sum == k){
+                sum += nums[j];
+                if(sum == k){
                     count++;
                 }
             }
@@ -24,3 +22,27 @@ public:
 
 
 //! ====================================== Optimal Approach =======================================
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        int sum = 0;
+
+        // sum - freq pairs
+        unordered_map<int,int> PreSumMap;
+        PreSumMap[0] = 1;
+
+        for(int i=0; i<n; i++){
+            sum += nums[i];
+
+            int rem = sum - k;
+            count += PreSumMap[rem];
+
+            PreSumMap[sum]++;
+        }
+
+        return count;
+    }
+};
