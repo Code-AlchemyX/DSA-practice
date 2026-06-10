21348dc70a39d8d65aa000084048f8fa57d8f0ee
Nayan <codingonly2029@gmail.com>
2026-06-10 12:05:02 +0530

    .

--- 06_Arrays/3_Hard/09_find_the_repeating_and_missing_number.cpp
+++ 06_Arrays/3_Hard/09_find_the_repeating_and_missing_number.cpp
@@ -74,7 +74,73 @@ class Solution {
 //! ============================================ Better Approach 2 ============================================
 //* Hashing
 
+class Solution {
+public:
+    vector<int> findTwoElement(vector<int>& arr) {
+        int n = arr.size();
+        
+        int hash[n+1] = {0};
+        
+        int repeating = -1;
+        int missing = -1;
+
+        for(int i=0; i<n; i++){
+            hash[arr[i]]++;    
+        }
+        
+        for(int i=1; i<=n; i++){
+            if(hash[i] == 0){
+                missing = i;
+            }
+            else if(hash[i] == 2){
+                repeating = i;
+            }
+            
+            if( repeating != -1 && missing != -1 ){
+                break;
+            }
+        }
+
+        return {repeating, missing};
+    }
+};
 
 
-//! ================================================ Optimal Approach ================================================
+//! ================================================ Optimal Approach 1 ================================================
 
+class Solution {
+  public:
+    vector<int> findTwoElement(vector<int>& arr) {
+        long long n = arr.size();
+        
+        long long total_sum = (n*(n+1))/2;
+        long long total_sum_of_sqaures = (n*(n+1)*(2*n+1))/6;
+        
+        long long array_sum = 0;
+        long long array_sum_of_sqaures = 0;
+        
+        for(int i=0; i<n; i++){
+            array_sum += arr[i];
+            array_sum_of_sqaures += (long long)(arr[i])*(long long)(arr[i]);
+        }
+        
+    //!------------------ Must check striver's lec for the maths done below ------------------
+        
+        // array_sum - total_sum = x - y  -----> From observation.   
+
+        long long val1 = array_sum - total_sum;   // x-y
+        long long val2 = array_sum_of_sqaures - total_sum_of_sqaures;
+        val2 = val2/val1;   // x+y
+        
+        long long x = (val1 + val2)/2;
+        long long y = x - val1;
+        
+        return {(int)x , int(y)};
+        
+    }
+};
+
+//! ================================================ Optimal Approach 2 ================================================
+//* XOR method
+
+  //todo --> Must know Bit Manipulation.
