e5f4129de7e14440e7f85048c02ccfb8f6a2325e
Nayan <codingonly2029@gmail.com>
2026-06-13 15:59:56 +0530

    Update 11_find_out_how_many_times_the_array_is_rotated.cpp

---
+++ 07_Binary_Search/1_BS_on_1D_arrays/11_find_out_how_many_times_the_array_is_rotated.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        int n = arr.size();
+        int low = 0;
+        int high = n-1;
+        int ans = INT_MAX;
+        int index = 0;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            
+            if(arr[low] <= arr[high]){
+                if(arr[low] < ans){
+                    ans = arr[low];
+                    index = low;
+                }
+                break;
+            }
+            
+            if(arr[low] <= arr[mid]){
+                if(arr[low] < ans){
+                    ans = arr[low];
+                    index = low;
+                }
+                low = mid +1;
+            }
+            else {
+                if(arr[mid] < ans){
+                    ans = arr[mid];
+                    index = mid;
+                }
+                high = mid -1;
+            }
+        }
+        
+        return index;
+    }
+};
