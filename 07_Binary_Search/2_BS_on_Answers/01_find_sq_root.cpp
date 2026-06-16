9123b450c9cfe2a5161d25041761c248bd6d0659
Nayan <codingonly2029@gmail.com>
2026-06-16 11:40:44 +0530

    .

---
+++ 07_Binary_Search/2_BS_on_Answers/01_find_sq_root.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        int ans = 1;
+        int low = 1;
+        int high = n;
+        
+        while(low <= high){
+            int mid = (low + high)/2;
+            
+            if( mid*mid <= n){
+                ans = mid;
+                low = mid +1;
+            }
+            else {
+                high = mid -1;
+            }
+        }
+        
+        return ans;
+    }
+};
