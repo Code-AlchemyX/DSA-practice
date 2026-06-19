60389c3ff75ef431cc266b127f394e2ec74c9c00
Nayan <codingonly2029@gmail.com>
2026-06-19 09:49:39 +0530

    Update 05_find_the_smallest_divisor_given_threshold.cpp

--- 07_Binary_Search/2_BS_on_Answers/05_find_the_smallest_divisor_given_threshold.cpp
+++ 07_Binary_Search/2_BS_on_Answers/05_find_the_smallest_divisor_given_threshold.cpp
@@ -1,22 +1,23 @@
 //! Brute Force
+
 class Solution {
 public:
-    int smallestDivisor(vector<int>& arr, int limit) {
-        int n = arr.size(); 
-        int maxi = *max_element(arr.begin(), arr.end());
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int n = nums.size();
+        int maxi = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            maxi = max(maxi, nums[i]);
+        }
 
         for (int d = 1; d <= maxi; d++) {
             int sum = 0;
-
             for (int i = 0; i < n; i++) {
-                sum += ceil((double)(arr[i]) / (double)(d));
+                sum += ceil((double)(nums[i]) / (double)(d));
             }
-
-            if (sum <= limit) {
+            if (sum <= threshold) {
                 return d;
-            } 
+            }
         }
-
         return -1;
     }
 };
@@ -24,3 +25,34 @@ public:
 
 //! Optimal Approach
 
+class Solution {
+public:
+    int sumByD(vector<int> &arr, int div) {
+        int sum = 0;
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
+            //sum = sum + ceil((double)(arr[i]) / (double)(div));   OR --> 
+            sum += (arr[i] + div - 1) / div;
+        }
+        return sum;
+    }
+
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int n = nums.size();
+
+        //* If the qs says return -1 then -->
+        // if (n > threshold) return -1;
+
+        int low = 1, high = *max_element(nums.begin(), nums.end());
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sumByD(nums, mid) <= threshold) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
