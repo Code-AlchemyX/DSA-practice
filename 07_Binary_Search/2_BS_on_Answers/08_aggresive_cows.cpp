ed18aa143496205255013435fdb61ccfdcb0067a
Nayan <codingonly2029@gmail.com>
2026-08-01 14:18:30 +0530

    Update 08_aggresive_cows.cpp

--- 07_Binary_Search/2_BS_on_Answers/08_aggresive_cows.cpp
+++ 07_Binary_Search/2_BS_on_Answers/08_aggresive_cows.cpp
@@ -35,3 +35,41 @@ class Solution {
 
 //! ========================================== Optimal Approach ==========================================
 
+class Solution {
+  public:
+    bool canWePlace(vector<int>& stalls , int dist , int cows){
+        int n = stalls.size();
+        int cntCows = 1;
+        int last = stalls[0];
+        
+        for(int i=1; i<n; i++){
+            if(stalls[i] - last >= dist){
+                cntCows++;
+                last = stalls[i];
+            }
+            if(cntCows >= cows) return true;
+        }
+        return false;
+    }
+    
+    int aggressiveCows(vector<int> &stalls, int k) {
+        int n = stalls.size();
+        sort(stalls.begin() , stalls.end());
+        
+        int low = 1;
+        int high = stalls[n-1] - stalls[0];
+        int ans = 0;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (canWePlace(stalls, mid, k) == true) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
