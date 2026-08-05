8a0647327cc216ffeb69edf55364ece2d604c1c5
Nayan <codingonly2029@gmail.com>
2026-08-05 16:34:52 +0530

    Update 04_longest_common_prefix.cpp

--- 08_Strings_[Basic & Medium]/01_Basic_and_easy_string_problems/04_longest_common_prefix.cpp
+++ 08_Strings_[Basic & Medium]/01_Basic_and_easy_string_problems/04_longest_common_prefix.cpp
@@ -21,3 +21,27 @@ public:
         return strs[0].substr(0, size_of_min_string);
     }
 };
+
+
+// ========================================================================================
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& str) {
+        if (str.empty()) return "";
+
+        sort(str.begin(), str.end());
+
+        string first = str[0];
+        string last = str[str.size() - 1];
+        string ans = "";
+
+        int minLength = min(first.size(), last.size());
+        for (int i = 0; i < minLength; i++) {
+            if (first[i] != last[i]) break;
+            ans += first[i];
+        }
+        return ans;
+    }
+};
