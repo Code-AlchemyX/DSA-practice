46beba6d85f5bf388480d6f1744f8da7e6a76241
Nayan <codingonly2029@gmail.com>
2026-08-10 18:12:07 +0530

    .

--- 09_Linked_List/02_Doubly_LL/03_delete_head_of_DLL.cpp
+++ 09_Linked_List/02_Doubly_LL/03_delete_head_of_DLL.cpp
@@ -1,3 +1,41 @@
+//! Easy solution --> striver's lec
+
+/* Structure of doubly linked list Node
+class Node {
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+    
+    Node(int x) {
+        data = x;
+        next = nullptr;
+        prev = nullptr;
+    }
+};*/
+class Solution {
+    public:
+    Node* deleteHead(Node* head) {
+        if(head == nullptr) return nullptr;
+        if(head->next == nullptr){
+            delete head;
+            return nullptr;
+        }
+        
+        Node* prevHead = head;
+        head = head->next;
+        head->prev = nullptr;
+        prevHead->next = nullptr;
+        delete prevHead;
+        
+        return head;
+    }
+};
+
+//! ====================================================================================
+
+//! A bit confusing solution for newbies.
+
 /* Structure of doubly linked list Node
 class Node {
 public:
@@ -11,6 +49,7 @@ public:
         prev = nullptr;
     }
 };*/
+
 class Solution {
   public:
     Node* deleteHead(Node* head) {
