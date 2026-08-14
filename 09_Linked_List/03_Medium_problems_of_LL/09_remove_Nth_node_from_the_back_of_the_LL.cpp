7b2d38af635a3bbec4dee36245c643bc317374b3
Nayan <codingonly2029@gmail.com>
2026-08-14 23:19:42 +0530

    .

---
+++ 09_Linked_List/03_Medium_problems_of_LL/09_remove_Nth_node_from_the_back_of_the_LL.cpp
@@ -0,0 +1,50 @@
+//! Brute force --> Two pass method
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* temp = head;
+        int size = 0;
+
+        while(temp != nullptr){
+            size++;
+            temp = temp->next;
+        }
+
+        if(size == n){
+            ListNode* newHead = head->next;
+            delete head;
+            return newHead;
+        }
+
+        int node_from_front = size - n;
+
+        ListNode* prev = head;
+        for(int i=1; i<node_from_front; i++){
+            prev = prev->next;
+        }
+
+        ListNode* nodeToDelete = prev->next;
+        prev->next = prev->next->next;
+        delete nodeToDelete;
+
+        return head;
+    }
+};
+
+
+//! Optimal approach 
+
+
+
