65e002067a3c34d5ea9a9b564c8147e5efba391a
Nayan <codingonly2029@gmail.com>
2026-08-14 14:43:30 +0530

    .

--- 09_Linked_List/03_Medium_problems_of_LL/07_check _if_LL_is_palindrome.cpp
+++ 09_Linked_List/03_Medium_problems_of_LL/07_check _if_LL_is_palindrome.cpp
@@ -86,3 +86,58 @@ public:
         return true;
     }
 };
+
+
+
+
+
+//! The code above is OPTIMAL too.... but in an interview changing the given input is considered as bad practice and hence we return our original array after palindrome check.
+class Solution {
+private:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* nextnode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextnode;
+        }
+        return prev;
+    }
+
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+
+        // 1. Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // 2. Reverse second half
+        ListNode* newHead = reverseList(slow->next);
+
+        // 3. Compare halves
+        ListNode* left = head;
+        ListNode* right = newHead;
+        bool result = true;
+
+        while (right != nullptr) {
+            if (left->val != right->val) {
+                result = false;
+                break;
+            }
+            left = left->next;
+            right = right->next;
+        }
+
+        // 4. Restore original list
+        slow->next = reverseList(newHead);
+
+        return result;
+    }
+};
