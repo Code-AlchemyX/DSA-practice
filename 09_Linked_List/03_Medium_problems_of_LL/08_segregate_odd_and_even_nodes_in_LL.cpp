a5ffe1aa0a21639f1ebda5a78c522b7eea9e66ea
Nayan <codingonly2029@gmail.com>
2026-08-15 18:39:54 +0530

    .

--- 09_Linked_List/03_Medium_problems_of_LL/08_segregate_odd_and_even_nodes_in_LL.cpp
+++ 09_Linked_List/03_Medium_problems_of_LL/08_segregate_odd_and_even_nodes_in_LL.cpp
@@ -48,5 +48,61 @@ public:
 //! Brute Force 2
 
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        // Base case: empty list or single element
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+
+        vector<int> arr;
+        ListNode* temp = head;
+
+        // 1. Collect all odd-indexed nodes (1st, 3rd, 5th, ...) 
+        while (temp != NULL && temp->next != NULL) {
+            arr.push_back(temp->val);
+            temp = temp->next->next;
+        }
+        if (temp != NULL) {
+            arr.push_back(temp->val);
+        }
+
+        // 2. Collect all even-indexed nodes (2nd, 4th, 6th, ...) 
+        temp = head->next;
+        while (temp != NULL && temp->next != NULL) {
+            arr.push_back(temp->val);
+            temp = temp->next->next;
+        }
+        if (temp != NULL) {
+            arr.push_back(temp->val);
+        }
+
+        // 3. Re-assign values back into the linked list 
+        temp = head;
+        int i = 0;
+        while (temp != NULL) {
+            temp->val = arr[i];
+            i++;
+            temp = temp->next;
+        }
+
+        return head;
+    }
+};
+
+
+
 //! Optimal approach
+
 
