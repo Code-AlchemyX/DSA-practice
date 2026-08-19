05e6eee1c162e86ea7fb233178a5df8e1acb304b
Nayan <codingonly2029@gmail.com>
2026-08-19 20:29:39 +0530

    .

--- 09_Linked_List/03_Medium_problems_of_LL/13_find_the_intersection_point_of_Y_LL.cpp
+++ 09_Linked_List/03_Medium_problems_of_LL/13_find_the_intersection_point_of_Y_LL.cpp
@@ -1,4 +1,4 @@
-//! Brute Force
+//! Brute Force --> TC = O(m*n)  and  SC = O(1)
 
 /**
  * Definition for singly-linked list.
@@ -11,23 +11,111 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp1 = headA;
-
-        while(temp1 != nullptr){
-            ListNode* temp2 = headB;
-
-            while(temp2 != nullptr){
-                if(temp1 == temp2){
-                    return temp1;
+        ListNode *tempA = headA;
+        
+        while (tempA != nullptr) {
+            ListNode *tempB = headB;
+            
+            while (tempB != nullptr) {
+                if (tempA == tempB) {
+                    return tempA;
                 }
-                temp2 = temp2->next;
+                tempB = tempB->next;
             }
-
-            temp1 = temp1->next;
-        } 
-
+            
+            tempA = tempA->next;
+        }
+        
         return nullptr;
     }
 };
+
+//! Better approach --> TC = O(m+n)   SC = O(m)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_set<ListNode*> visited;
+        
+        ListNode *tempA = headA;
+        while (tempA != nullptr) {
+            visited.insert(tempA);
+            tempA = tempA->next;
+        }
+        
+        ListNode *tempB = headB;
+        while (tempB != nullptr) {
+            if (visited.count(tempB)) {
+                return tempB;
+            }
+            tempB = tempB->next;
+        }
+        
+        return nullptr;
+    }
+};
+
+//! Optimal Approach 1  --> TC = O(m+n)  SC = O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* tempA = headA;
+        ListNode* tempB = headB;
+        int sizeA = 0, sizeB = 0;
+
+        while (tempA != nullptr) {
+            sizeA++;
+            tempA = tempA->next;
+        }
+        while (tempB != nullptr) {
+            sizeB++; 
+            tempB = tempB->next;
+        }
+
+        tempA = headA;
+        tempB = headB;
+
+        int size_diff = abs(sizeA - sizeB);
+        if (sizeA > sizeB) {
+            for (int i = 0; i < size_diff; i++) {
+                tempA = tempA->next;
+            }
+        } else {
+            for (int i = 0; i < size_diff; i++) {
+                tempB = tempB->next;
+            }
+        }
+
+        while (tempA != tempB) {
+            tempA = tempA->next;
+            tempB = tempB->next;
+        }
+
+        return tempA;
+    }
+};
+
+
+//! Optimal Approach 2
+
+
+
 
 
