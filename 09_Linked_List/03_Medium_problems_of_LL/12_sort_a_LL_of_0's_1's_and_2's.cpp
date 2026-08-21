6dbab62a009e03debb0fec036fd76bbdd074d812
Nayan <codingonly2029@gmail.com>
2026-08-21 17:49:10 +0530

    .

--- 09_Linked_List/03_Medium_problems_of_LL/12_sort_a_LL_of_0's_1's_and_2's.cpp
+++ 09_Linked_List/03_Medium_problems_of_LL/12_sort_a_LL_of_0's_1's_and_2's.cpp
@@ -1,3 +1,39 @@
+//! Brute Force
+
+Node* segregate(Node* head) {
+    if (!head || !head->next) return head;
+
+    int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+    Node* temp = head;
+
+    while (temp != nullptr) {
+        if (temp->data == 0) cnt0++;
+        else if (temp->data == 1) cnt1++;
+        else cnt2++;
+        temp = temp->next;
+    }
+
+    temp = head;
+
+    while (temp != nullptr) {
+        if (cnt0 > 0) {
+            temp->data = 0;
+            cnt0--;
+        } else if (cnt1 > 0) {
+            temp->data = 1;
+            cnt1--;
+        } else {
+            temp->data = 2;
+            cnt2--;
+        }
+        temp = temp->next;
+    }
+
+    return head;
+}
+
+
+
 //! Optimal 1 --> Self derived
 
 /* Node is defined as
