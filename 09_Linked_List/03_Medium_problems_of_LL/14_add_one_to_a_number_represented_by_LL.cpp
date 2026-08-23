58aa24efac1c5e53aa48fa7eedfda10cc41096c0
Nayan <codingonly2029@gmail.com>
2026-08-23 11:43:50 +0530

    .

--- 09_Linked_List/03_Medium_problems_of_LL/14_add_one_to_a_number_represented_by_LL.cpp
+++ 09_Linked_List/03_Medium_problems_of_LL/14_add_one_to_a_number_represented_by_LL.cpp
@@ -113,3 +113,49 @@ class Solution {
 
 //! Optimal approach 
 
+/* Structure of linked list Node
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    int sum(Node* temp){
+        if(temp == nullptr){
+            return 1;
+        }
+        
+        int carry = sum(temp->next);
+        
+        temp->data = temp->data + carry;
+    
+        if(temp->data < 10){
+            return 0;
+        }
+        
+        temp->data = 0;
+        
+        return 1;
+    }
+    
+    Node* addOne(Node* head) {
+        Node* temp = head;
+        
+        int carry = sum(temp);
+        
+        if(carry == 1){
+            Node* newNode = new Node(1);
+            newNode->next = head;
+            return newNode;
+        }
+        
+        return head;
+    }
+};
