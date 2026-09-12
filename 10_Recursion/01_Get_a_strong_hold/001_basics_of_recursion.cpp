00d4684e4c4bc79ff69457e9a5d4b5b8da3fe2e9
Nayan <codingonly2029@gmail.com>
2026-09-12 14:20:06 +0530

    .

--- 10_Recursion/01_Get_a_strong_hold/001_basics_of_recursion.cpp
+++ 10_Recursion/01_Get_a_strong_hold/001_basics_of_recursion.cpp
@@ -83,3 +83,124 @@ using namespace std;
 
 //     return 0;
 // }
+
+
+//!  Print the sum of first n natural numbers --> Method 1
+// void sum(int i, int n, int &total_sum){
+//     //* Base case
+//     if(i > n) return;
+
+//     total_sum += i;
+//     sum(i+1 , n , total_sum);
+// }
+
+// int main(){
+//     int n;
+//     cout << "Enter n: " << endl;
+//     cin >> n;
+    
+//     int total_sum = 0;
+//     int i = 0;
+//     sum(i , n , total_sum);
+
+//     cout << total_sum << endl;
+
+//     return 0;
+// }
+
+
+//!  Print the sum of first n natural numbers --> Method 2
+// void func(int n , int sum){
+//     //* Base case
+//     if(n < 1){
+//         cout << sum << endl;
+//         return;
+//     }
+
+//     func(n-1 , sum + n);
+// }
+
+// int main(){
+//     int n;
+//     cout << "Enter n: " << endl;
+//     cin >> n;
+    
+//     int sum = 0;
+//     func(n, sum);
+
+//     return 0;
+// }
+
+
+//! Factorial of n --> Method 1 (counting from n to 1)
+
+// void func(int n, int &fact) {
+//     if (n <= 1) {
+//         return;
+//     }
+
+//     fact *= n;
+//     func(n - 1, fact);
+// }
+
+// int main() {
+//     int n;
+//     cout << "Enter n: ";
+//     cin >> n;
+
+//     if (n < 0) {
+//         return 1;
+//     }
+
+//     int fact = 1;
+//     func(n, fact);
+
+//     cout << fact << endl;
+//     return 0;
+// }
+
+
+//! Factorial of n --> Method 2 (counting from 1 to n)
+
+// void func(int i, int &fact , int n) {
+//     if(i > n){
+//         return;
+//     }
+
+//     func(i+1 , fact , n);
+//     fact *= i;
+// }
+
+// int main() {
+//     int n;
+//     cout << "Enter n: ";
+//     cin >> n;
+
+//     if (n < 0) {
+//         return 1;
+//     }
+
+//     int fact = 1;
+//     int i = 1;
+//     func(i, fact , n);
+
+//     cout << fact << endl;
+//     return 0;
+// }
+
+
+//! Factorial of n --> Method 3
+
+// int func(int n) {
+//     if(n == 0) return 1;
+//     return n*func(n-1);
+// }
+
+// int main() {
+//     int n;
+//     cout << "Enter n: ";
+//     cin >> n;
+
+//     cout << func(n) << endl;
+//     return 0;
+// }
