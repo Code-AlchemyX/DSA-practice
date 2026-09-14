512f8b90e1ab5908131fde4712fbaa907d0e5e76
Nayan <codingonly2029@gmail.com>
2026-09-14 10:50:40 +0530

    .

--- 10_Recursion/02_Subsequence_Pattern/02_generate_parantheses.cpp
+++ 10_Recursion/02_Subsequence_Pattern/02_generate_parantheses.cpp
@@ -1,3 +1,58 @@
+//! Brute Force
+
+class Solution {
+public:
+
+    bool isValid(string s) {
+        int balance = 0;
+
+        for(char ch : s) {
+            if(ch == '(')
+                balance++;
+            else
+                balance--;
+
+            if(balance < 0)
+                return false;
+        }
+
+        return balance == 0;
+    }
+
+    void generate(string s, vector<string>& result, int count, int n) {
+
+        if(count == 2 * n) {
+            if(isValid(s))
+                result.push_back(s);
+
+            return;
+        }
+
+        s.push_back('(');
+        generate(s, result, count + 1, n);
+        s.pop_back();
+
+        s.push_back(')');
+        generate(s, result, count + 1, n);
+        s.pop_back();
+    }
+
+    vector<string> generateParenthesis(int n) {
+
+        vector<string> result;
+        string s;
+
+        generate(s, result, 0, n);
+
+        return result;
+    }
+};
+
+
+
+//! Optimal Approach 
+
+
 class Solution {
 public:
     void generate(string s , vector<string> &result , int left , int right){
